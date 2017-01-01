b0d679e5847828b1e22efc371f9017433bcd2568
docsis <docsis.tt@gmail.com>
2017-01-01 23:08:34 +0800

    ...

---
+++ algs3/p1_2.c
@@ -0,0 +1,24 @@
+/* quick union */
+#include <stdio.h>
+
+#define N 1000
+
+int main(int argc, char *argv[])
+{
+	int i, j, p, q, t, id[N];
+	
+	for (i=0; i<N; i++)
+		id[i] = i;
+	
+	while (scanf("%d %d\n", &p, &q) == 2) {
+		for (i=p; i!=id[i]; i=id[i])
+			;
+		for (j=q; j!=id[j]; j=id[j])
+			;
+		if (i == j)
+			continue;
+
+		id[i] = j;
+		printf(" %d %d\n", p, q);
+	}
+}
