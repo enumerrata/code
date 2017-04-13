032ff1a8095faed102c7180014efab046a26812d
docsis <docsis.tt@gmail.com>
2017-04-13 22:06:28 +0800

    ...

---
+++ leetcode/300/207_courseschedule.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include <limits.h>
+
+#include "../tree.h"
+
+bool canFind(int** prerequisites, int prerequisitesRowSize, int pos, bool *isfinding, bool *canfind) {
+    if (canfind[pos]) {
+        return true;
+    }
+    isfinding[pos] = true;
+    int i;
+    for (i = 0 ; i < prerequisitesRowSize ; i++) {
+        if (prerequisites[i][0] == pos) {
+            int pre = prerequisites[i][1];
+            if (isfinding[pre]) {
+                // loop appears
+                return false;
+            }
+            canfind[pre] = canFind(prerequisites, prerequisitesRowSize, pre, isfinding, canfind);
+            if (!canfind[pre]) {
+                return false;
+            }
+        }
+    }
+    isfinding[pos] = false;
+    return true;
+}
+bool canFinish(int numCourses, int** prerequisites, int prerequisitesRowSize, int prerequisitesColSize) {
+    bool isfinding[numCourses], canfind[numCourses];
+    memset(isfinding, false, sizeof(bool) * numCourses); // whether is finding
+    memset(canfind, false, sizeof(bool) * numCourses);   // whether the sourse can be started
+    int i;
+    for (i = 0 ; i < numCourses ; i++) {
+        if (!canFind(prerequisites, prerequisitesRowSize, i, isfinding, canfind)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main(int argc, char *argv[])
+{
+	printf("> \n");
+
+	return 0;
+}
+
