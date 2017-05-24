0b77cb1f21a47ff9181bb98b419d749bbdf96a01
docsis <docsis.tt@gmail.com>
2017-05-24 21:40:55 +0800

    ...

--- leetcode/dp/198_houserobber.c
+++ leetcode/dp/198_houserobber.c
@@ -12,20 +12,21 @@ int max(int a, int b)
     return a>b?a:b;
 }
 
+/* keep rob & norob max value */
 int rob(int* nums, int numsSize) {
-    int a = 0;
-    int b = nums[0];
+    int r = 0;
+    int nr = 0;
+    int t;
     int i;
     
-    for (i=1; i<numsSize; i++) {
-        int t;
-        t = b;
-        b = max(a+nums[i], b);
-        a = t;
+    for (i=0; i<numsSize; i++) {
+        t = r;
+        r = nr + nums[i];
+        nr = max(t, nr);
     }
-    return max(a,b);
+ 
+    return max(r, nr);   
 }
-
 
 int main(int argc, char *argv[])
 {
