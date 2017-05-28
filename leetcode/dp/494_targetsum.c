9d4ce26d781b15c0a145f56277cca1585650166b
docsis <docsis.tt@gmail.com>
2017-05-28 20:13:21 +0800

    ...

--- leetcode/dp/494_targetsum.c
+++ leetcode/dp/494_targetsum.c
@@ -8,6 +8,7 @@
 #include "../tree.h"
 
 
+#if 0
 void dfs(int *nums, int d, int sum, int S, int *cnt, int numsSize)
 {
     if (d == numsSize) {
@@ -27,6 +28,34 @@ int findTargetSumWays(int* nums, int numsSize, int S) {
     
     return cnt;
 }
+#endif
+
+int subsetSum(int *nums, int siz, int s) {
+    int dp[s+1]; 
+    int i,n;
+    
+    memset(dp, 0, sizeof(int)*(s+1));
+    dp[0] = 1;
+    for (n = 0; n < siz; n++)
+        for (int i = s; i >= nums[n]; i--)
+            dp[i] += dp[i - nums[n]]; 
+    return dp[s];
+} 
+
+int findTargetSumWays(int* nums, int numsSize, int S) {
+    int i,j;
+    int sum=0;
+
+    for (i=0; i<numsSize; i++) {
+        sum += nums[i];
+    }
+    if (sum < S || (sum + S)%2)
+        return 0;
+    // subset sum = (sum+s)/2
+    
+    return subsetSum(nums, numsSize, (sum+S)/2);
+}
+
 int main(int argc, char *argv[])
 {
 	printf("> \n");
