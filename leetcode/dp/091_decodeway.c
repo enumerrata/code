42d7b2e1fb44427be97334fa8a79e85d72896a21
docsis <docsis.tt@gmail.com>
2017-06-01 21:10:21 +0800

    ...

--- leetcode/dp/091_decodeway.c
+++ leetcode/dp/091_decodeway.c
@@ -4,7 +4,7 @@
 #include <stdbool.h>
 #include <ctype.h>
 
-
+#if 0
 int isv(char a) {
     return a <= '9' && a >= '1';
 }
@@ -42,7 +42,36 @@ int numDecodings(char* s) {
 
     return w1;
 }
+#endif
+int numDecodings(char* s) {
+    int cnt = 1;
+    int len = strlen(s);
+    int dp[len+1];
+    int i;
 
+    if (len < 1)
+        return 0;
+    
+    if (s[0] == '0')
+        return 0;
+
+    dp[0] = 1;
+    dp[1] = 1;
+
+    for (i=2; i<len+1; i++) {
+        dp[i] = 0;
+
+        if (s[i-1]>'0')
+            dp[i] += dp[i-1];
+
+        if (s[i-2] == '1' || s[i-2] == '2' && s[i-1]<='6') {
+            dp[i] += dp[i-2];
+        }
+    }
+    //printf("%d %d %d\n", dp[0], dp[1], dp[2]);
+    return dp[len];
+
+}
 int main(int argc, char *argv[])
 {
 	printf("> \n");
