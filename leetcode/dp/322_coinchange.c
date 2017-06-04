8fd930c99e0a4a14ab14c791e632e27248280eae
docsis <docsis.tt@gmail.com>
2017-06-04 23:28:42 +0800

    ...

--- leetcode/dp/322_coinchange.c
+++ leetcode/dp/322_coinchange.c
@@ -7,6 +7,7 @@
 
 #include "../tree.h"
 
+#if 0
 int min(int a, int b)
 {
 	return a>b?b:a;
@@ -36,6 +37,37 @@ int coinChange(int* coins, int coinsSize, int amount) {
     }  
     return dp[amount] == 0x7fffffff ? -1 : dp[amount];  
 }
+#endif
+
+
+int mini(int a, int b)
+{
+    return a>b?b:a;
+}
+
+int coinChange(int* coins, int coinsSize, int amount) {
+    int dp[amount+1];
+    int i,j;
+    int min;
+    
+    dp[0] = 0;
+    for (i=1; i<amount+1; i++) {
+        min = INT_MAX;
+        for (j=0;  j<coinsSize; j++) {
+            if (coins[j]<=i && dp[i-coins[j]] >= 0) {
+                min = mini(dp[i-coins[j]]+1, min);
+            } 
+        }
+		dp[i] = (min == INT_MAX?-1:min);
+    }
+#if 0
+	for (i=0; i<amount; i++) {
+		printf("%03d ", dp[i]);
+	}
+#endif
+    return dp[amount];
+}
+
 int main(int argc, char *argv[])
 {
 	int a[] = {1,2,5};
