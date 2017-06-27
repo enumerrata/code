4cce4acd36897623cf3080c54cdf3fb7584cf2af
docsis <docsis.tt@gmail.com>
2017-06-27 21:57:22 +0800

    ...

--- leetcode/401_binarywatch.c
+++ leetcode/401_binarywatch.c
@@ -4,6 +4,9 @@
 #include <stdbool.h>
 #include <ctype.h>
 
+
+#if 1
+
 /**
  * Return an array of size *returnSize.
  * Note: The returned array must be malloced, assume caller calls free().
@@ -44,13 +47,59 @@ char** readBinaryWatch(int num, int* returnSize) {
     return p;
 }
 
+#else
+
+/**
+ * Return an array of size *returnSize.
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+
+void gen(int num, int cur, int siz, int *a) {
+	int i,j,t;
+
+	if (siz == 0) {
+		for (i=0; i<10; i++)
+			printf("%d ", a[i]);
+		printf("\n", a[i]);
+		return;
+	}
+
+	for (i=cur; i<10; i++) {
+		a[i] = 1;
+		gen(num, i+1, siz-1, a);
+		a[i] = 0;
+	}
+		
+}
+
+char** readBinaryWatch(int num, int* returnSize) {
+    char **p;
+    int *siz = returnSize;
+    int i;
+	int a[10] = {0};
+
+    *siz = 0;
+    p = malloc(sizeof(char *) * 1000);
+    
+    if (!num) {
+        p[(*siz)++] = strdup("0:00");
+        return p;
+    }
+    
+	gen(10, 0, num, a);
+    
+}
+#endif
+
 int main(int argc, char *argv[])
 {
-	int ret;
+	int ret,i;
+	char **p;
 
-	readBinaryWatch(2, &ret);
+	p = readBinaryWatch(2, &ret);
 
-	printf("> %d\n", ret);
+	for (i=0; i<ret; i++)
+		printf("> %s\n", p[i]);
 
 	return 0;
 }
